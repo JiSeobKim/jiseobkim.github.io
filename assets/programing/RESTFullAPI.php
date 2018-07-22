a81d0f97cc9353a9dd341c6319fcb157d269c208
JiseobKim <jiseob812@gmail.com>
2018-07-22 14:55:07 +0900

    웹파일 저장

--- assets/programing/RESTFullAPI.php
+++ assets/programing/RESTFullAPI.php
@@ -1,15 +1,19 @@
-<!DOCTYPE html>
-<html>
-<body>
-  <?php
+<?php
 
-    $response = returnValue();
+  $response = Array();
 
-    $response['hp'] = '01012341234';
-    $response['name'] = 'KJS';
-    $response['age']
+  $response['hp'] = '01012341234';
+  $response['name'] = 'KJS';
+  $response['age'] = 27;
+  $response['data1'] = "data1";
+  $response['data2'] = "data2";
+  $response['data3'] = "data3";
+  $response['data4'] = "data4";
+  $response['data5'] = "data5";
+  $response['data6'] = "data6";
+  $response['data7'] = "data7";
+  $response['data8'] = "data8";
 
-    echo json_encode($response,JSON_UNESCAPED_UNICODE);
-  ?>
-</body>
-</html>
+
+  echo json_encode($response,JSON_UNESCAPED_UNICODE);
+?>
