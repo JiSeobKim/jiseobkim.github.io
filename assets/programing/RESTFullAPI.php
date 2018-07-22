771672a2df0b9bb4c7759bbad080dc237e0878b4
JiseobKim <jiseob812@gmail.com>
2018-07-22 14:54:10 +0900

    임시저장

---
+++ assets/programing/RESTFullAPI.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+  <?php
+
+    $response = returnValue();
+
+    $response['hp'] = '01012341234';
+    $response['name'] = 'KJS';
+    $response['age']
+
+    echo json_encode($response,JSON_UNESCAPED_UNICODE);
+  ?>
+</body>
+</html>
